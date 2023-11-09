18ffa7c8da9f957bb2cf017c708e0dc1242c2113
Niklas Bartelheimer <bartelheimer@em.uni-frankfurt.de>
2023-11-09 13:29:07 +0100

    format

--- src/devices/bxi-portals/GPI2_BXI_SEG.c
+++ src/devices/bxi-portals/GPI2_BXI_SEG.c
@@ -26,7 +26,7 @@ along with GPI-2. If not, see <http://www.gnu.org/licenses/>.
 #include "GPI2.h"
 #include "GPI2_BXI.h"
 
-int _pgaspi_dev_unregister_mem(gaspi_rc_mseg_t* seg){
+int _pgaspi_dev_unregister_mem(gaspi_rc_mseg_t* seg) {
 	int ret;
 
 	if (!PtlHandleIsEqual(seg->data_md_handle, PTL_INVALID_HANDLE)) {
@@ -77,7 +77,7 @@ int pgaspi_dev_register_mem(gaspi_context_t const* const gctx,
 	seg->notify_spc_md_handle = PTL_INVALID_HANDLE;
 	seg->notify_spc_le_handle = PTL_INVALID_HANDLE;
 
-	PtlGetUid(portals4_dev_ctx->ni_handle,&uid);
+	PtlGetUid(portals4_dev_ctx->ni_handle, &uid);
 
 	if (PTL_OK != ret) {
 		GASPI_DEBUG_PRINT_ERROR("PtlGetUid failed with %d", ret);
@@ -85,52 +85,54 @@ int pgaspi_dev_register_mem(gaspi_context_t const* const gctx,
 		return -1;
 	}
 
-	if(seg->data.buf == NULL){
-	PORTALS4_DEBUG_PRINT_MSG("Allocating Data Segment at with size %d",seg->size);
+	if (seg->data.buf == NULL) {
+		PORTALS4_DEBUG_PRINT_MSG("Allocating Data Segment at with size %d",
+		                         seg->size);
 
-	memset(&md,0,sizeof(ptl_md_t));
-	memset(&le,0,sizeof(ptl_le_t));
+		memset(&md, 0, sizeof(ptl_md_t));
+		memset(&le, 0, sizeof(ptl_le_t));
 
-	md.start = seg->data.buf;
-	md.length = seg->size;
-	md.options = PTL_MD_EVENT_SUCCESS_DISABLE | PTL_MD_EVENT_CT_SEND |
-	             PTL_MD_EVENT_CT_REPLY | PTL_MD_EVENT_CT_ACK;
-	md.eq_handle = portals4_dev_ctx->data_eq_handle;
-	md.ct_handle = portals4_dev_ctx->data_ct_handle;
+		md.start = seg->data.buf;
+		md.length = seg->size;
+		md.options = PTL_MD_EVENT_SUCCESS_DISABLE | PTL_MD_EVENT_CT_SEND |
+		             PTL_MD_EVENT_CT_REPLY | PTL_MD_EVENT_CT_ACK;
+		md.eq_handle = portals4_dev_ctx->data_eq_handle;
+		md.ct_handle = portals4_dev_ctx->data_ct_handle;
 
-	ret = PtlMDBind(portals4_dev_ctx->ni_handle, &md, &seg->data_md_handle);
+		ret = PtlMDBind(portals4_dev_ctx->ni_handle, &md, &seg->data_md_handle);
 
-	if (PTL_OK != ret) {
-		GASPI_DEBUG_PRINT_ERROR("PtlMDBind failed with %d", ret);
-		_pgaspi_dev_unregister_mem(seg);
-		return -1;
-	}
+		if (PTL_OK != ret) {
+			GASPI_DEBUG_PRINT_ERROR("PtlMDBind failed with %d", ret);
+			_pgaspi_dev_unregister_mem(seg);
+			return -1;
+		}
 
-	le.start = seg->data.buf;
-	le.length = seg->size;
-	le.options = PTL_LE_OP_PUT | PTL_LE_OP_GET | PTL_LE_EVENT_SUCCESS_DISABLE |
-	             PTL_LE_EVENT_LINK_DISABLE;
-	le.uid = uid;
+		le.start = seg->data.buf;
+		le.length = seg->size;
+		le.options = PTL_LE_OP_PUT | PTL_LE_OP_GET |
+		             PTL_LE_EVENT_SUCCESS_DISABLE | PTL_LE_EVENT_LINK_DISABLE;
+		le.uid = uid;
 
-	ret = PtlLEAppend(portals4_dev_ctx->ni_handle,
-	                  portals4_dev_ctx->data_pt_index,
-	                  &le,
-	                  PTL_PRIORITY_LIST,
-	                  NULL,
-	                  &seg->data_le_handle);
-	if (PTL_OK != ret) {
-		GASPI_DEBUG_PRINT_ERROR("PtlLEAppend failed with %d", ret);
-		_pgaspi_dev_unregister_mem(seg);
-		return -1;
-	}
+		ret = PtlLEAppend(portals4_dev_ctx->ni_handle,
+		                  portals4_dev_ctx->data_pt_index,
+		                  &le,
+		                  PTL_PRIORITY_LIST,
+		                  NULL,
+		                  &seg->data_le_handle);
+		if (PTL_OK != ret) {
+			GASPI_DEBUG_PRINT_ERROR("PtlLEAppend failed with %d", ret);
+			_pgaspi_dev_unregister_mem(seg);
+			return -1;
+		}
 	}
 	if (seg->notif_spc.buf != NULL) {
-		PORTALS4_DEBUG_PRINT_MSG("Allocating Notify Segment at with size %d",seg->size);
+		PORTALS4_DEBUG_PRINT_MSG("Allocating Notify Segment at with size %d",
+		                         seg->size);
 
-		memset(&md,0,sizeof(ptl_md_t));
-		memset(&le,0,sizeof(ptl_le_t));
-		
-		PORTALS4_DEBUG_PRINT_MSG("UID is %d",uid);
+		memset(&md, 0, sizeof(ptl_md_t));
+		memset(&le, 0, sizeof(ptl_le_t));
+
+		PORTALS4_DEBUG_PRINT_MSG("UID is %d", uid);
 
 		md.start = seg->notif_spc.buf;
 		md.length = seg->notif_spc_size;
