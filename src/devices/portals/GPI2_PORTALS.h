a0655a42b2563354581ef376e23f9cc0de3d0752
bartelheimer1 <bartelheimer1@deepv.deep>
2024-01-10 14:14:32 +0100

    implemented eager protocol version off passive; restructured device context struct

--- src/devices/portals/GPI2_PORTALS.h
+++ src/devices/portals/GPI2_PORTALS.h
@@ -22,52 +22,39 @@ along with GPI-2. If not, see <http://www.gnu.org/licenses/>.
 #include <portals4.h>
 #include "GPI2_Dev.h"
 
-#define PORTALS4_ME_PT_INDEX 0
-#define PORTALS4_ME_MATCH_BITS 0xDEADBEEF
-#define PORTALS4_ME_IGNORE_BITS 0
 #define PORTALS4_EVENT_SLOTS (1024)
 #define PORTALS4_ACK_TYPE PTL_CT_ACK_REQ
 #define PORTALS4_PASSIVE_ACK_TYPE PTL_ACK_REQ
 
-#ifdef DEBUG
-#define PORTALS4_DEBUG_PRINT_MSG(msg, ...)       \
-	{                                            \
-		fprintf(stdout,                          \
-		        "Message at (%s:%d): " msg "\n", \
-		        __FILE__,                        \
-		        __LINE__,                        \
-		        ##__VA_ARGS__);                  \
-	}
-#endif
-
-enum pt_states {PT_FREE = 0, PT_ALLOCATED = 1};
+enum pt_states { PT_FREE = 0, PT_ALLOCATED = 1 };
 
 typedef struct {
 	ptl_handle_md_t group_md;
 	ptl_handle_md_t passive_md;
 	ptl_handle_md_t comm_md[GASPI_MAX_QP];
 	ptl_handle_le_t le_handle;
-	ptl_pt_index_t  pt_index;
+	ptl_pt_index_t pt_index;
 } portals4_mr;
 
 struct portals4_ctx_info {
 	ptl_process_t phys_address;
-	ptl_process_t matching_phys_address;
 };
 
 typedef struct {
 	ptl_handle_ni_t ni_handle;
-	ptl_handle_ni_t match_ni_handle;
 	ptl_handle_eq_t eq_handle;
-	ptl_handle_eq_t passive_rcv_eq_handle;
 	ptl_handle_ct_t group_ct_handle;
-	ptl_handle_ct_t passive_snd_ct_handle;
 	ptl_handle_ct_t comm_ct_handle[GASPI_MAX_QP];
-	ptl_pt_index_t  me_pt_index; // passive comm matching interface
-	//ptl_handle_me_t me_handle; // passive comm matching interface
+	struct {
+		ptl_handle_eq_t eq_handle;
+		ptl_handle_ct_t ct_handle;
+		ptl_handle_le_t le_handle;
+		ptl_pt_index_t pt_index;
+		unsigned char* msg_buf;
+	} passive_comm;
 	struct portals4_ctx_info* local_info;
 	struct portals4_ctx_info* remote_info;
-	int8_t *pte_states;
+	int8_t* pte_states;
 	int max_ptes;
 } gaspi_portals4_ctx;
 
