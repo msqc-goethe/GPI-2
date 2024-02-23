da1cc9aa4f2fb0ec4fa10f9d72314a32ea21c898
Niklas Bartelheimer <bartelheimer@em.uni-frankfurt.de>
2024-02-23 13:26:57 +0100

    removed old defaults

--- src/GPI2_CONFIG.c
+++ src/GPI2_CONFIG.c
@@ -22,11 +22,7 @@ along with GPI-2. If not, see <http://www.gnu.org/licenses/>.
 #include <portals4.h>	
 #endif
 #define GASPI_MAX_GROUPS (32)
-#ifdef GPI2_DEVICE_PORTALS
 #define GASPI_MAX_MSEGS (255)
-#else
-#define GASPI_MAX_MSEGS (255)
-#endif
 #define GASPI_MAX_QSIZE (4096)
 #ifdef GPI2_DEVICE_PORTALS
 #define GASPI_MAX_TSIZE_C (1ul << 26ul)
